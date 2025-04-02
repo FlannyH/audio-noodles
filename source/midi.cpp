3235e247508c7b02bfeaf20fe7d9f21bc6d33ee1
Lily Haverlag <flannyha@gmail.com>
2025-04-02 02:13:26 +0200

    Parse midi messages

--- source/midi.cpp
+++ source/midi.cpp
@@ -5,10 +5,42 @@ namespace Midi {
     std::shared_ptr<RtMidiIn> midi_in;
 
     void midi_message_callback(double delta_time, std::vector<unsigned char>* message, void* user_data) {
-        for (size_t i = 0; i < message->size(); ++i) {
-            printf ("%02X ", message->at(i));
+        const uint8_t type = (message->at(0) >> 4) & 0x07;
+
+        if (type == 0) {
+            const uint8_t key = message->at(1);
+            const uint8_t velocity = message->at(2);
+            printf("Note Off: key %i, velocity %i\n", key, velocity);
         }
-        printf("\n");
+        else if (type == 1) {
+            const uint8_t key = message->at(1);
+            const uint8_t velocity = message->at(2);
+            printf("Note On: key %i, velocity %i\n", key, velocity);
+        }
+        else if (type == 2) {
+            const uint8_t key = message->at(1);
+            const uint8_t pressure = message->at(2);
+            printf("Polyphonic Aftertouch: key %i, pressure %i\n", key, pressure);
+        }
+        else if (type == 3) {
+            const uint8_t controller = message->at(1);
+            const uint8_t data = message->at(2);
+            printf("Control Change: controller %i, data %i\n", controller, data);
+        }
+        else if (type == 4) {
+            const uint8_t program = message->at(1);
+            printf("Program Change: program %i\n", program);
+        }
+        else if (type == 5) {
+            const uint8_t pressure = message->at(1);
+            printf("Channel Aftertouch: pressure %i\n", pressure);
+        }
+        else if (type == 6) {
+            const uint16_t lsb = message->at(1);
+            const uint16_t msb = message->at(2);
+            const uint16_t pitch_wheel = (msb << 8) + lsb;
+            printf("Pitch Wheel: %i\n", pitch_wheel);
+        }
     }
 
     void init() {
