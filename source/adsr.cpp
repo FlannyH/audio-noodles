3ac90679b333b4d7cabeeaa803e1f4092dc83188
Lily Haverlag <flannyha@gmail.com>
2025-04-04 18:12:23 +0200

    Add ADSR

---
+++ source/adsr.cpp
@@ -0,0 +1,72 @@
+#include "adsr.hpp"
+#include <cmath>
+#include <cstdio>
+
+void VolEnv::tick(double delta_time, const VolEnvParams& params) {
+    this->stage_time += delta_time;
+
+    if (this->stage == VolEnvStage::delay) {
+        this->adsr_volume = 0.0;
+        if (this->stage_time >= params.delay) {
+            this->stage_time -= params.delay;
+            this->stage = VolEnvStage::attack;
+        }
+    }
+    
+    if (this->stage == VolEnvStage::attack) {
+        if (params.attack == 0.0) {
+            this->stage = VolEnvStage::hold;
+        }
+        else {
+            this->adsr_volume = std::min(this->stage_time / params.attack, 1.0);
+            if (this->stage_time >= params.attack) {
+                this->stage_time -= params.attack;
+                this->stage = VolEnvStage::hold;
+            }
+        }
+    }
+    
+    if (this->stage == VolEnvStage::hold) {
+        this->adsr_volume = 1.0;
+        if (this->stage_time >= params.hold) {
+            this->stage_time -= params.hold;
+            this->stage = VolEnvStage::decay;
+        }
+    }
+    
+    if (this->stage == VolEnvStage::decay) {
+        if (params.decay == 0) {
+            this->stage = VolEnvStage::sustain;
+        }
+        else {
+            this->adsr_volume = std::lerp(1.0, params.sustain, this->stage_time / params.decay);
+            if (this->stage_time >= params.decay) {
+                this->stage_time -= params.decay;
+                this->stage = VolEnvStage::sustain;
+            }
+        }
+    }
+    
+    if (this->stage == VolEnvStage::sustain) {
+        this->adsr_volume = params.sustain;
+        if (params.sustain == 0.0) {
+            this->stage = VolEnvStage::idle;
+            this->adsr_volume = 0.0;
+        }
+    }
+    
+    if (this->stage == VolEnvStage::release) {
+        if (params.release == 0.0) {
+            this->stage = VolEnvStage::idle;
+            this->adsr_volume = 0.0;
+        }
+        else {
+            this->adsr_volume -= params.release * delta_time;
+            if (this->adsr_volume <= 0.0) {
+                this->stage = VolEnvStage::idle;
+                this->adsr_volume = 0.0;
+                this->stage_time = 0.0;
+            }
+        }
+    }
+}
---
+++ source/adsr.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include <cstdint>
+
+enum class VolEnvStage {
+    idle = 0,
+    delay,
+    attack,
+    hold,
+    decay,
+    sustain,
+    release,
+    n_env_stages,
+};
+
+struct VolEnvParams {
+    double delay = 0.0;   // Delay stage length in seconds
+    double attack = 0.002;  // Attack stage length in seconds
+    double hold = 0.0;    // Hold stage length in seconds
+    double decay = 0.7;   // Decay stage length in seconds
+    double sustain = 0.0; // Sustain volume between 0.0 and 1.0
+    double release = 2.0; // Release stage in volume units per second
+};
+
+struct VolEnv {
+    double adsr_volume = 0.0;
+    double stage_time = 0.0; // Time in seconds how long we've been at this envelope stage
+    VolEnvStage stage = VolEnvStage::idle; // What stage we're at now
+
+    void tick(double delta_time, const VolEnvParams& params);
+};
