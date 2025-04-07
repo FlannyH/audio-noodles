da05e08f2ba7d3ad051f16dd8aa9106d4df0fb93
Lily Haverlag <flannyha@gmail.com>
2025-04-07 21:23:58 +0200

    Initialize graphics system
    yippeeee blank window achieved

--- source/audio_noodle.cpp
+++ source/audio_noodle.cpp
@@ -2,12 +2,18 @@
 #include "midi.hpp"
 #include "mixer.hpp"
 #include "session.hpp"
+#include "graphics/renderer.hpp"
 
 int main() {
     Midi::init();
     Mixer::init();
+    Gfx::init(Gfx::RenderAPI::OpenGL, 1280, 720, "Audio Noodles");
     Session::tracks().push_back(Track{});
-    while (1) {
+    while (Gfx::should_stay_open()) {
+        Gfx::begin_frame();
+        Gfx::end_frame();
+
+        // todo: move this to separate thread
         Midi::process();
     };
 }
