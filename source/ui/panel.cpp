de5d3d4abb0bf6a83fb597cb0845cb769c1730a7
Lily Haverlag <flannyha@gmail.com>
2025-04-12 00:13:02 +0200

    Display panel name in window bar

--- source/ui/panel.cpp
+++ source/ui/panel.cpp
@@ -4,8 +4,14 @@
 namespace UI {
 
     void Panel::update(float delta_time) {
-        constexpr float window_bar_height = 32.0f;
+        constexpr float window_bar_height = 40.0f;
         scene.update_extents(this->top_left, this->size, window_bar_height);
+        Gfx::draw_text_pixels(
+            name, (Gfx::TextDrawParams){
+                      .transform       = {.position = glm::vec3(this->top_left + glm::vec2(4.0f, 6.0f), 0.0f), .scale = {2.0f, 2.0f, 1.0f}},
+                      .position_anchor = Gfx::AnchorPoint::TopLeft,
+                      .color           = Colors::WHITE,
+                  });
         Gfx::draw_rectangle_2d_pixels( // Title bar border
             this->top_left, this->top_left + glm::vec2(this->size.x, window_bar_height + 2),
             (Gfx::DrawParams){.anchor_point = Gfx::AnchorPoint::TopLeft, .rectangle_outline_width = 2.0f});
--- source/ui/panel.hpp
+++ source/ui/panel.hpp
@@ -6,6 +6,7 @@
 namespace UI {
     struct Panel {
         Scene scene;
+        std::string name = "Yippee panels have names now :3";
         glm::vec2 top_left;
         glm::vec2 size;
         glm::vec4 bg_color = glm::vec4(0.1f, 0.1f, 0.2f, 1.0f);
